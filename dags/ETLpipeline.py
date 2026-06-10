632a2055d71eca23bdd60b3f997677a3b3247899
hiteshhacks <paratehitesh45@gmail.com>
2026-06-10 15:35:52 +0530

    ETL pipeline completed

---
+++ dags/ETLpipeline.py
@@ -0,0 +1,87 @@
+from airflow import DAG
+from airflow.providers.http.operators import SimpleHttpOperator
+from datetime import datetime
+from airflow.decorators import task 
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+import json
+from airflow.utils.dates import days_ago
+
+# define DAG
+with DAG(
+    dag_id="ETL_pipeline",
+    start_date=days_ago(1),
+    scgedule="@daily",
+    catchup=False
+)as dag:
+    
+    # step 1: Create table if it does not exist
+    @task
+    def create_table():
+        # initialize Postgres hook
+        postgres_hook =  PostgresHook(postgres_conn_id="postgres_default")
+
+
+        # create table query
+        create_table_query= """
+        CREATE TABLE IF NOT EXISTS nasa_apod (
+           id SERIAL PRIMARY KEY,
+           title VARCHAR(255),
+           explanation TEXT,
+           url TEXT,
+           date DATE,
+           media_type VARCHAR(50)  
+           );
+"""
+
+        # execute query
+        postgres_hook.run(create_table_query)
+
+
+    # step 2: Extract data from NASA API
+
+    extract_apod=SimpleHttpOperator(
+        task_id="extract_apod",
+        http_con_id="nasa_api",  ## connection id defined in Airflow UI for nasa api
+        endpoint="planetary/apod", ## nasa api endpoint for astronomy picture of the day
+        method="GET",
+        data={"api_key":"{{ conn.nasa_api.extra_djson_api_key }}"}, ## use the api key from the connection configuration in Airflow UI
+        reponse_filter=lambda response: response.json()
+    )
+
+
+
+    # step 3: Transform data (pick the information we need to save )
+
+    @task
+    def transform_data(response):
+        apod_data ={
+            "title":response.get("title",""),
+            "explanation":response.get("explanation",""),
+            "url":response.get("url",""),
+            "date":response.get("date",""),
+            "media_type":response.get("media_type","")
+        }
+        return apod_data
+    
+
+
+
+    # step 4: Load data into Postgres database
+    @task
+    def load_data_to_postgres(apod_data):
+        postgres_hook = PostgresHook(postgres_conn_id="postgres_default")
+
+        insert_query= """
+        INSERT INTO nasa_apod (title, explanation, url, date, media_type)
+        VALUES (%s, %s, %s, %s, %s);   
+
+"""
+        postgres_hook.run(insert_query, parameters=(
+            apod_data["title"],
+            apod_data["explanation"],
+            apod_data["url"],
+            apod_data["date"],
+            apod_data["media_type"]
+        ))
+    # step 5: verify data in DBviewer
+    # step6 : set dependencies between tasks
